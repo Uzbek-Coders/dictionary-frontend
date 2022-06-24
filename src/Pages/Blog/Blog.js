9833ee92e12fb23a526aea6e52a4341d41a7f184
Asqar Arslonovm <asqararslonov2008@gmail.com>
2022-06-24 11:45:49 +0500

    first commit

---
+++ src/Pages/Blog/Blog.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Post_con from "./Post_container/Post_con";
+
+export const Blog = () => {
+  return (
+    <div>
+      <Post_con/>
+    </div>
+  );
+};
---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "./components/NavBar/NavBar";
+import './app.css'
+
+
+const App = () => {
+    return(
+        <div className="app">
+            <NavBar />
+        </div>
+    );
+}
+
+export default App;
